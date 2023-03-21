f19667c62a14a540a1b9f24f86d0fe5c47cc38d4
Osajie Omongbale <majorosajie@gmail@gmail.com>
2023-03-20 19:33:03 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+ lines (10 sloc)  141 Bytes
+
+#include <stdio.h>
+
+/**
+ * main - print source file name
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
