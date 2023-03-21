3de8e7792b09ea7f3408ab39bd5afe8fdc2ee214
Osajie Omongbale <majorosajie@gmail@gmail.com>
2023-03-20 19:54:55 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,12 @@
- lines (10 sloc)  141 Bytes
-
 #include <stdio.h>
 
 /**
- * main - print source file name
- * Return: always 0
+ * main - prints the name of the file.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-   fprintf("%s\n", __FILE__);
+	fprintf("%s\n", __FILE__);
 	return (0);
 }
